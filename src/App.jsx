b7e4ef23fb12a929b474902cbb170f8e3d807565
fullyoo19 <fullyoo19@gmail.com>
2025-11-24 11:00:04 +0900

    타이틀 수정

--- src/App.jsx
+++ src/App.jsx
@@ -119,7 +119,7 @@ function App() {
   return (
 
     <div className='app'>
-      <h1 className='title'>Todo List</h1>
+      <h1 className='title'>Todo List(할일)</h1>
       <div className='contents'>
 
         {/* 인풋 추가 영역  */}
